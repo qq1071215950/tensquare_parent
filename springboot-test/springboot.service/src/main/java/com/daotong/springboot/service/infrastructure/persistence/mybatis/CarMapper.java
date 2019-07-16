b7f5425d8e56d0f083e688474c35d3624d17bf20
0027008030@ztesoft.com <CHensir888518>
2019-07-16 16:18:41 +0800

    modify swagger版本修正

--- springboot-test/springboot.service/src/main/java/com/daotong/springboot/service/infrastructure/persistence/mybatis/CarMapper.java
+++ springboot-test/springboot.service/src/main/java/com/daotong/springboot/service/infrastructure/persistence/mybatis/CarMapper.java
@@ -1,10 +1,8 @@
 package com.daotong.springboot.service.infrastructure.persistence.mybatis;
 
 import com.daotong.springboot.service.domain.repository.CarDO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.mapstruct.Mapper;
-
-import java.util.List;
 
 /**
  * @author 陈井彬
